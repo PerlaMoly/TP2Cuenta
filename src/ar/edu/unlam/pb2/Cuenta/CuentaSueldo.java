f8ebd4b9617380c0fffd3a3fa82f8717c50bf3ec
analiafer <fernandezanalia39@gmail.com>
2019-05-14 21:46:08 -0300

    commit analia

---
+++ src/ar/edu/unlam/pb2/Cuenta/CuentaSueldo.java
@@ -0,0 +1,18 @@
+package ar.edu.unlam.pb2.Cuenta;
+
+public class CuentaSueldo {
+      
+	private Double cantidad;
+
+	public Double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public CuentaSueldo(){
+		
+	}
+}
